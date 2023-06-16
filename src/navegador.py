10142c64b6a0834c4dabbb8c982d041f4217ae9b
Calebe Prates <eltoncalebeprates@gmail.com>
2023-06-15 23:19:26 -0300

    teste de melhoria

--- src/navegador.py
+++ src/navegador.py
@@ -10,22 +10,26 @@ user_agent =  ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
 'Chrome/39.0.2171.95 Safari/537.36')
 
 options = Options()
+options.add_argument('--headless')
 options.add_argument("start-maximized")
 options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('window-size=1920,1080')
 options.add_argument(f'user-agent={user_agent}')
+options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--disable-dev-shm-using") 
+options.add_argument("--disable-extensions") 
+options.add_argument("--disable-gpu") 
+options.add_argument("disable-infobars")
+options.add_argument("--no-sandbox") 
+options.add_argument("--disable-setuid-sandbox") 
 
 def capture_data_in_link(links: list):
   for link in links:
     print(capture_data_company('https://cadastroempresa.com.br'+link))
     with open("retorno_24.txt", "a") as file:
       file.write(str(capture_data_company('https://cadastroempresa.com.br'+link))+'\n')
-
-
-options = Options()
-options.add_argument('--headless')
-options.add_argument('window-size=1920,1080')
 
 navegador = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 navegador.get('https://cadastroempresa.com.br/procura?q=abaete')
